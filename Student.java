e699398ca4486ae92ad215cbac0dfabf1c81d357
AnastasijaVasilevska <anastasija.vasilevska6@gmail.com>
2020-04-11 18:52:04 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
+	List<Integer> labPoints = Arrays.asList(1,2,3,4,5,6,7,8,9,10)
 
 	//TODO constructor
 
