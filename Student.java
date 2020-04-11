696d4fff58816b10f0aa7fb47fe0631bc83d1a6b
AnastasijaVasilevska <anastasija.vasilevska6@gmail.com>
2020-04-11 19:00:41 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,20 +1,70 @@
+import sun.font.TrueTypeGlyphMapper;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Student {
 	String index;
 	String firstName;
 	String lastName;
-	List<Integer> labPoints = Arrays.asList(1,2,3,4,5,6,7,8,9,10)
+	List<Integer> labPoints = new ArrayList<Integer>(10)
 
 	//TODO constructor
 
+	public Student() {
+	}
+
+
 	//TODO seters & getters
+
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setLabPoints(List<Integer> labPoints) {
+		this.labPoints = labPoints;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public List<Integer> getLabPoints() {
+		return labPoints;
+	}
 
 	public double getAverage() {
 		//TODO
+		suma=0
+		for(int i=0;i<getLabPoints().length;i++){
+			suma+=getLabPoints()
+		}
+		return suma/getLabPoints().length
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		if(getLabPoints().length>8){
+			return true
+		}
+		else
+			return false
 	}
 }
